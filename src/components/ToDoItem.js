127d80b19927bc0dc49441c4dcdd23cb4a89f273
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-01-30 22:48:04 +0330

    Improved the handle click function

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,20 +3,16 @@ import { useState } from "react";
 export default function ToDoItem(props) {
   const [isDone, setIsDone] = useState(false);
 
-  function handelClick() {
-    if (isDone === false) {
-      setIsDone(true);
-    } else if (isDone === true) {
-      setIsDone(false);
-    }
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
   }
 
   return (
     <li
-      onClick={handelClick}
-      style={
-        isDone ? { textDecoration: "line-through" } : { textDecoration: "none" }
-      }
+      onClick={handleClick}
+      style={{ textDecoration: isDone ? "line-through" : "none" }}
     >
       {props.text}
     </li>
